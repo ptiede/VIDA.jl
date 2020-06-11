d2357c401af5b1db7bf358d6d587a952c054e6bc
Paul <ptiede91@gmail.com>
2020-06-11 16:12:55 -0400

    Moved plotting into recipes

--- src/visualizations.jl
+++ src/visualizations.jl
@@ -1,26 +1,36 @@
 
 
-function setfonts(fontsize=12)
-    PyPlot.matplotlib.rc("mathtext",fontset="cm")        #computer modern font
-    PyPlot.matplotlib.rc("font",family="serif",size=fontsize)  #font similar to LaTeX
-    PyPlot.matplotlib.rc("text", usetex=true)
+
+@recipe function f(image::AbstractImage)
+    size --> (500,400)
+    xaxis --> "RA (pixel)"
+    yaxis --> "DEC (pixel)"
+    seriescolor --> :afmhot
+    aspect_ratio := 1
+    xitr = 1:image.nx
+    yitr = 1:image.ny
+    bar_width --> 0
+    xlims --> (0,image.nx)
+    ylims --> (0,image.ny)
+    left_margin --> -5mm
+    right_margin --> 5mm
+    x := xitr
+    y := yitr
+    z := Surface(image.img.*1000.0)
+    seriestype := heatmap
+    fontfamily --> "serif"
+    colorbar_title --> "mJy/pixel"
+    colorbar --> :top
+    widen := false
+    framestyle --> :box
+    #margin --> 0mm
+
+    ()
 end
 
-"""
-Plots the image being read in.
-"""
-function PyPlot.plot(image::AbstractImage)
-    setfonts()
-    fig,ax = PyPlot.subplots(1)
-    ax.set_aspect(:equal)
-    ax.set_xlabel("RA (pixel)")
-    ax.set_ylabel("DEC (pixel)")
-    pim = ax.imshow(image.img, cmap=:afmhot,
-                   vmin=0.0, vmax=maximum(image.img), origin="lower")
-    fig.colorbar(pim, ax=ax, label="Jy/pixel")
 
-    return fig
-end
+
+
 
 
 
@@ -31,32 +41,325 @@ where `image` is templated off of EHTImage struct.
 
 # Details
 This was created to be close to the ehtim display object. It takes an
-EHTImage object and plots it using PyPlot, returning the figure.
+EHTImage object and plots it according to EHT conventions.
 
 Note that is does not save the figure.
-
 """
-function PyPlot.plot(image::T; xlim=(-50,50), ylim=(-50,50)) where {T<:EHTImage}
-    setfonts()
+@recipe function f(image::EHTImage)
+
+    #Define some constants
     μas2rad = π/180.0/3600/1e6
     #Construct the image grid in μas
     psizeuas_x = -image.psize_x
     psizeuas_y = image.psize_y
     fovx = psizeuas_x*image.nx
     fovy = psizeuas_y*image.ny
-    brightness_temp = image.wavelength^2/(2*k_B.val)/1e26/abs(μas2rad^2*image.psize_x*image.psize_y)
-    fig,ax = PyPlot.subplots(1)
-    pim = ax.imshow(image.img*brightness_temp/1e9, cmap=:afmhot,
-                    extent = [fovx/2,-fovx/2, -fovy/2, fovy/2],
-                    vmin=0.0, vmax=maximum(image.img)*brightness_temp/1e9,
-                    origin="lower")
-    ax.set_xlabel(L"$\Delta$RA  ($\mu$as)")
-    ax.set_ylabel(L"$\Delta$DEC ($\mu$as)")
-    fig.colorbar(pim,ax=ax, label=L"Brightness Temperature ($10^9$ K)")
-    title_leg = image.source*" "*string(round(c_0.val/image.wavelength/1e9))*" GHz"
-    ax.set_title(title_leg)
-    return fig
+    brightness_temp = image.wavelength^2/(2*k_B.val)/1e26/
+                      abs(μas2rad^2*image.psize_x*image.psize_y)
+
+    tickfontsize --> 11
+    guidefontsize --> 14
+    size --> (500,400)
+    xaxis --> "ΔRA  (μas)"
+    yaxis --> "ΔDEC (μas)"
+    seriescolor --> :afmhot
+    aspect_ratio --> 1
+    bar_width --> 0
+    xlims --> (-fovx/2,fovx/2)
+    ylims --> (-fovy/2,fovy/2)
+    #left_margin --> -2mm
+    right_margin --> 5mm
+    x := range(-fovx/2, fovx/2; length=image.nx)
+    y := range(-fovy/2, fovy/2; length=image.ny)
+    z := Surface(@view(image.img[:,end:-1:1])*brightness_temp/1e9)
+    seriestype := heatmap
+    fontfamily --> "sans serif"
+    colorbar_title --> "Brightness Temperature (10⁹ K)"
+    xflip --> true
+    widen := false
+    #framestyle --> :box
+    title --> image.source*" "*string(round(c_0.val/image.wavelength/1e9))*" GHz"
+    linecolor-->:black
+    tick_direction --> :out
+    ()
 end
+
+@userplot Triptic
+"""
+    $(SIGNATURES)
+A plot recipe for a triptic plot with the `image`, i.e. an EHT image and the filter `\theta`
+used for parameter estimate. The first two panels are the image and filter and
+the third are RA and DEC cross-sections of both the filter and image centered at the
+center of light.
+"""
+@recipe function f(h::Triptic)
+    if length(h.args) != 2 || !(typeof(h.args[1]) <: EHTImage) ||
+        !(typeof(h.args[2]) <: AbstractFilter)
+        error("Triptic should be given a image and filter.  Got: $(typeof(h.args))")
+    end
+    image, θ = h.args
+    #link := :both
+    widen := false
+    xflip := true
+    framestyle := [:none,:none,:axes]
+    grid := false
+    size --> (825,300)
+    layout := @layout [a b c]
+    left_margin --> -2mm
+    right_margin --> 2mm
+    bottom_margin --> 3mm
+    tickfontsize --> 10
+
+
+    #Construct the image grid in μas
+    psizeuas_x = -image.psize_x
+    psizeuas_y = image.psize_y
+    fovx = psizeuas_x*image.nx
+    fovy = psizeuas_y*image.ny
+    xitr = range(-fovx/2, fovx/2; length=image.nx)
+    yitr = range(-fovy/2, fovy/2; length=image.ny)
+    dataim = @view(image.img[:,end:-1:1])/sum(image.img)
+
+    #Construct the filter image
+    fxitr, fyitr, fimg = filter_image(θ, image.nx, [-fovx/2,fovx/2], [-fovy/2,fovy/2])
+    fimg .= fimg*abs(psizeuas_x*psizeuas_y)
+    fimg .= fimg/sum(fimg)
+
+
+    #Get scale bar and slice data.
+    size = 40#μas
+    startx = fovx/2*(1-0.1)
+    starty = -fovy/2*(1-0.2)
+    xseg = range(startx, startx-size, length=4)
+    yseg = starty*ones(4)
+    #Finally the slices
+    xcol,ycol = center_of_light(collect(xitr), collect(yitr), fimg)
+    xx = collect(xitr)
+    yy = collect(yitr)
+    xcol,ycol = center_of_light(reverse(xitr),yitr,fimg)
+    imin = argmin(abs.(xx .- xcol))
+    jmin = argmin(abs.(yy .- ycol))
+
+    #Image plot
+    @series begin
+        widen := false
+        xflip --> true
+        seriestype := :heatmap
+        xlims --> (-fovx/2, fovx/2)
+        ylims --> (-fovy/2, fovy/2)
+        framestyle := :none
+        seriescolor --> :afmhot
+        colorbar := false
+        subplot := 1
+        #size := (300,300)
+        aspect_ratio := 1
+        xflip := true
+        x := xitr
+        y := yitr
+        z := Surface(dataim)
+        ()
+
+    end
+
+    @series begin
+        subplot := 1
+        seriestype := :hline
+        legend := false
+        linestyle := :solid
+        framestyle := :none
+        seriescolor := :cornflowerblue
+        y := ycol
+        widen := false
+        aspect_ratio := 1
+        linewidth := 2
+        ()
+    end
+
+    @series begin
+        subplot := 1
+        seriestype := :vline
+        legend := false
+        linestyle := :solid
+        framestyle := :none
+        seriescolor := :red
+        y := xcol
+        widen := false
+        aspect_ratio := 1
+        linewidth := 2
+        ()
+    end
+
+
+    @series begin
+        fontsize --> 10
+        aspect_ratio := 1
+        subplot := 1
+        seriestype := :line
+        seriescolor := :white
+        linewidth := 1
+        annotations := [(startx-size/2,starty-4, "40 μas", font(10,color=:white)),
+                        (startx-size/4, -starty+4, "Image", font(10,color=:white))]
+        fontcolor := :white
+        xlims = (-fovx/2,fovx/2)
+        ylims = (-fovy/2,fovy/2)
+        x := xseg
+        y := yseg
+        legend := false
+        framestyle := :none
+        left_margin := -2mm
+        right_margin := -2mm
+        ()
+    end
+
+    #Now we move onto the filter Image
+    @series begin
+        widen := false
+        xflip --> true
+        seriestype := :heatmap
+        xlims --> (-fovx/2, fovx/2)
+        ylims --> (-fovy/2, fovy/2)
+        framestyle := :none
+        seriescolor --> :afmhot
+        colorbar := false
+        subplot := 2
+        aspect_ratio := 1
+        x := xitr
+        y := yitr
+        z := Surface(@view(fimg[:,end:-1:1]))
+        ()
+
+    end
+
+    @series begin
+        subplot := 2
+        seriestype := :hline
+        legend := false
+        linestyle := :dash
+        framestyle := :none
+        aspect_ratio := 1
+        seriescolor := :cornflowerblue
+        y := ycol
+        widen := false
+        linewidth := 2
+        ()
+    end
+
+    @series begin
+        subplot := 2
+        seriestype := :vline
+        legend := false
+        linestyle := :dash
+        framestyle := :none
+        xflip := true
+        aspect_ratio := 1
+        seriescolor := :red
+        y := xcol
+        widen := false
+        linewidth := 2
+        ()
+    end
+
+    #Now do the scale bar
+    @series begin
+        fontsize --> 10
+        subplot := 2
+        seriestype := :line
+        seriescolor := :white
+        linewidth := 1
+        aspect_ratio := 1
+        annotations := [(startx-size/2,starty-4, "40 μas", font(10,color=:white)),
+                        (startx-size/4, -starty+4, "Filter", font(10,color=:white))]
+        fontcolor := :white
+        xlims = (-fovx/2,fovx/2)
+        ylims = (-fovy/2,fovy/2)
+        x := xseg
+        y := yseg
+        xflip := true
+        legend := false
+        framestyle := :none
+        widen := false
+        ()
+    end
+
+    #Now the chord plot
+    @series begin
+        subplot := 3
+        seriestype := :line
+        linestyle := :solid
+        seriescolor := :cornflowerblue
+        x := reverse(xitr)
+        y := @view dataim[jmin,image.ny:-1:1]
+        legend := false
+        ()
+    end
+    #Now the chord plot
+    @series begin
+        subplot := 3
+        seriestype := :line
+        linestyle := :solid
+        seriescolor := :red
+        x := yitr
+        y := @view dataim[:, image.nx-imin]
+        xguide := "RA, DEC chords (μas)"
+        yticks := false
+        legend := false
+        ()
+    end
+    #Now the chord plot
+    @series begin
+        subplot := 3
+        seriestype := :line
+        linestyle := :dash
+        seriescolor := :cornflowerblue
+        x := xitr
+        y := @view fimg[jmin,image.ny:-1:1]
+        legend := false
+        ()
+    end
+    #Now the chord plot
+    @series begin
+        subplot := 3
+        seriestype := :line
+        seriescolor := :red
+        linestyle := :dash
+        x := yitr
+        y := fimg[:, image.nx-imin]
+        xguide := "RA, DEC chords (μas)"
+        yticks := false
+        legend := false
+        ()
+    end
+
+end
+
+@recipe function f(θ::AbstractFilter; npix=128, fovx=120, fovy=120)
+
+    tickfontsize --> 11
+    guidefontsize --> 14
+    size --> (400,400)
+    xaxis --> "ΔRA  (μas)"
+    yaxis --> "ΔDEC (μas)"
+    seriescolor --> :afmhot
+    aspect_ratio --> 1
+    bar_width --> 0
+    xlims --> (-fovx/2,fovx/2)
+    ylims --> (-fovy/2,fovy/2)
+    xitr,yitr,img = filter_image(θ, npix, [-fovx/2,fovx/2],[-fovy/2,fovy/2])
+    #left_margin --> -2mm
+    right_margin --> 5mm
+    x := reverse(xitr)
+    y := yitr
+    z := Surface(img[:,end:-1:1]/sum(img))
+    seriestype := heatmap
+    xflip --> true
+    widen := false
+    #framestyle --> :box
+    linecolor-->:black
+    tick_direction --> :out
+    colorbar --> false
+    ()
+end
+
 
 function make_ehtimage(θ::AbstractFilter,
                        npix::Int, xlim, ylim;
@@ -96,127 +399,4 @@ function make_ehtimage(θ::AbstractFilter,
                       source_img.dec, source_img.wavelength,
                       source_img.mjd, img*scale_norm)
     return imgeht
-end
-
-
-
-
-function PyPlot.plot(θ::T, npix::Int=60,
-                     xlim=[-40,40], ylim=[-40,40];
-                     kwargs...) where {T<:AbstractFilter}
-    fig,ax = PyPlot.subplots(1)
-    setfonts()
-
-    _,_,img = filter_image(θ, npix, xlim, ylim)
-    fovx = (xlim[2]-xlim[1])
-    fovy = (ylim[2]-ylim[1])
-    pim = ax.imshow(img, cmap=:afmhot,
-                    extent = [fovx/2,-fovx/2, -fovy/2, fovy/2],
-                    vmin=0.0, vmax=maximum(img),
-                    origin="lower"; kwargs...)
-    ax.set_xlabel(L"$\Delta$RA  ($\mu$as)")
-    ax.set_ylabel(L"$\Delta$DEC ($\mu$as)")
-
-    return fig
-end
-
-"""
-    $(SIGNATURES)
-A plotting function used for benchmarking.
-"""
-function plot_filter(measure, θ::T, p1, p2, n) where {T<:AbstractFilter}
-    ℓ = Matrix{Float64}(undef,n,n)
-    filter_benchmark!(ℓ, measure, θ, p1, p2, n)
-
-    println("Maximum likelihood: ", maximum(ℓ))
-    fig, ax = PyPlot.subplots(1)
-    pim = ax.imshow(ℓ, extent=[p1[1],p1[2],p2[1],p2[2]],
-                    origin="lower", cmap=:viridis)
-    ax.set_aspect(:equal,:box)
-    ax.set_xlabel("r0")
-    ax.set_ylabel("ξ")
-    ax.axvline(θ.r0, color="white")
-    ax.axhline(θ.ξ, color="white")
-    fig.colorbar(pim,ax=ax)
-    savefig("test.png")
-    #PyPlot.savefig("test.png")
-    return ℓ
-
-end
-
-"""
-    $(SIGNATURES)
-Plots a triptic plot with the `image`, i.e. an EHT image and the filter `\theta`
-used for parameter estimate. The first two panels are the image and filter and
-the third are RA and DEC cross-sections of both the filter and image.
-"""
-function plot_triptic(image::EHTImage, θ::AbstractFilter)
-    setfonts()
-    fig,ax = PyPlot.subplots(1,3, figsize=(7.9,3))
-    fig.subplots_adjust(left=0.01,right=0.98, top=0.98,
-                        bottom=0.16, wspace=0.05, hspace=0.01)
-
-    #Construct the image grid in μas
-    psizeuas_x = -image.psize_x
-    psizeuas_y = image.psize_y
-    fovx = psizeuas_x*image.nx
-    fovy = psizeuas_y*image.ny
-    dataim = image.img/sum(image.img)
-    pim = ax[1].imshow(dataim, cmap=:afmhot,
-                    extent = [fovx/2,-fovx/2, -fovy/2, fovy/2],
-                    origin="lower", vmin=0.0, vmax=maximum(dataim))
-    ax[1].text(0.15, 0.95, "Image", horizontalalignment=:center,
-               verticalalignment=:center, transform=ax[1].transAxes,
-               color=:white)
-    ax[1].set_yticks([]);ax[1].set_xticks([])
-
-    xitr,yitr,img = filter_image(θ, image.nx, [-fovx/2,fovx/2], [-fovy/2,fovy/2])
-    img .= img*abs(psizeuas_y*psizeuas_x)
-    img .= img/sum(img)
-    pim = ax[2].imshow(img, cmap=:afmhot,
-                    extent = [fovx/2,-fovx/2, -fovy/2, fovy/2],
-                    vmin=0.0, vmax=maximum(dataim),
-                    origin="lower")
-    ax[2].set_yticks([]);ax[2].set_xticks([])
-    ax[2].text(0.15, 0.95, "Filter", horizontalalignment=:center,
-               verticalalignment=:center, transform=ax[2].transAxes,
-               color=:white)
-    xx = collect(xitr)
-    yy = collect(yitr)
-    xcol,ycol = center_of_light(xitr,yitr,img)
-    imin = argmin(abs.(xx .- xcol))
-    jmin = argmin(abs.(yy .- ycol))
-    ax[1].axhline(xcol, color=:cornflowerblue)
-    ax[1].axvline(xcol, color=:C3)
-    ax[2].axhline(ycol, color=:cornflowerblue, linestyle="--")
-    ax[2].axvline(ycol, color=:C3, linestyle="--")
-    ax[3].plot(xx,@view(dataim[jmin,image.ny:-1:1]),
-               label = "RA chord", color=:cornflowerblue)
-    ax[3].plot(yy,@view(dataim[:,imin]),
-               label = "DEC chord", color=:C3)
-    ax[3].plot(xx,@view(img[jmin,image.ny:-1:1]), linestyle="--",
-               label = "RA chord", color=:cornflowerblue)
-    ax[3].plot(yy,@view(img[:,imin]), linestyle="--",
-               label = "DEC chord", color=:C3)
-    ax[3].set_yticks([])
-    ax[3].set_xlabel(L"X,Y chords ($\mu$as)")
-
-    #make the scale bar
-    size = 40#μas
-    startx = fovx/2*(1-0.1)
-    starty = -fovy/2*(1-0.2)
-    scale_bar1 = PyPlot.matplotlib.lines.Line2D((startx, startx-size),
-                       (starty,starty), color=:white, alpha=1.0,
-                       linewidth=1, markeredgecolor=:white)
-    scale_bar2 = PyPlot.matplotlib.lines.Line2D((startx, startx-size),
-                       (starty,starty), color=:white, alpha=1.0,
-                       linewidth=1, markeredgecolor=:white)
-    ax[1].add_artist(scale_bar1)
-    ax[1].text(0.1, 0.12, latexstring(size)*L"\,$\mu\mathrm{as}$",
-               transform=ax[1].transAxes, color="white")
-    ax[2].add_artist(scale_bar2)
-    ax[2].text(0.1, 0.12, latexstring(size)*L"\,$\mu\mathrm{as}$",
-               transform=ax[2].transAxes, color="white")
-
-    return fig
 end
