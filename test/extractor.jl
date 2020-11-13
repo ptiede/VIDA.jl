993c83956b8ddfb0f70de80217fef0ca827b0078
Paul <ptiede91@gmail.com>
2020-11-13 11:30:09 -0500

    extract

--- test/extractor.jl
+++ test/extractor.jl
@@ -13,7 +13,7 @@ include("common.jl")
     θ0 = SlashedGaussianRing(r0*1.05, σ*1.05, s*0.95, ξs, x0, y0) +
                             0.11*AsymGaussian(σ*1.5, τ*0.95, ξτ*1.1, x0, y0)
     prob = ExtractProblem(bh, θ0, lower, upper)
-    rθ,divmin = threaded_extractor(4, prob, Opt(Fminbox(BFGS())))
+    rθ,divmin = threaded_extractor(4, prob, Opt(Fminbox(LBFGS())))
     @test isapprox(unpack(θ), unpack(θ), rtol=ϵ)
 end
 
@@ -30,8 +30,8 @@ end
     θ0 = SlashedGaussianRing(r0*2, σ*1.5, s*1.1, 0.5, 0.0, 0.0) +
          0.5*AsymGaussian(5.0, 0.5, 0.25, 0.0, 0.0)
     prob = ExtractProblem(bh, θ0, lower, upper)
-    rθ,divmin = threaded_extractor(4, prob, BBO(tracemode=:silent))
+    rθ,divmin = threaded_extractor(4, prob, BBO(tracemode=:compact))
     prob2 = ExtractProblem(bh, rθ, lower, upper)
-    rθ,divmin = threaded_extractor(4, prob2, CMAES(verbosity=0,ftol=1e-20, cov_scale=0.01))
+    rθ,divmin = threaded_extractor(4, prob2, CMAES(verbosity=1,ftol=1e-20, cov_scale=0.01))
     @test isapprox(unpack(rθ), unpack(θ), rtol=1e-1)
 end
