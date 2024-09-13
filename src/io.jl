3b08b33db9c13dfb9fc64c8a80cd34220f7ff310
Paul Tiede <ptiede91@gmail.com>
2024-09-13 09:28:23 -0400

    Fix a docstring

--- src/io.jl
+++ src/io.jl
@@ -96,7 +96,7 @@ end
 
 Loads an hdf5 movie file where `filename` should be a HDF5 file.
 # Details
-This reads in a hdf5 movie file and outputs and EHTMovie object.
+This reads in a hdf5 movie file and outputs and VIDAMovie object.
 
 # Notes
 Currently this only works with movies created by *ehtim*. SMILI uses a different
