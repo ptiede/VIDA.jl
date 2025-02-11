d3058d50258d1da22197097bd5178635642bb704
Paul Tiede <ptiede91@gmail.com>
2025-02-11 14:49:32 -0500

    Make Optimization a weakdep

---
+++ ext/VIDAOptimizationExt.jl
@@ -0,0 +1,25 @@
+module VIDAOptimizationExt
+
+using VIDA
+using Optimization: OptimizationFunction, OptimizationProblem, solve
+using Random
+
+function VIDA.vida(prob::VIDAProblem, optimizer; rng=Random.default_rng(), init_params=nothing, unit_cube=true, kwargs...)
+    T = eltype(prob.div.img)
+    f, t, (lb, ub) = VIDA.build_opt(prob, unit_cube)
+    x0 = VIDA.initial_point(rng, T, t, init_params)
+    fopt = OptimizationFunction((x,p)->f(x), prob.autodiff)
+    optprob = OptimizationProblem(fopt, x0, nothing; lb=lb, ub=ub)
+    xopt, min =  _vida(fopt, t, optprob, optimizer; kwargs...)
+    return xopt, prob.f(xopt), min
+end
+
+
+function _vida(fopt, t, optprob, optimizer; kwargs...)
+    sol = solve(optprob, optimizer; kwargs...)
+    xopt = transform(t, sol.u)
+    return xopt, sol.objective
+end
+
+
+end
